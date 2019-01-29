5788b582c7c9b4d11ff67cd9dcddf03164ce966b
Brayan Zavala <brynzvl93@gmail.com>
2019-01-28 23:30:53 -0800

    Commiting JS Files

---
+++ calculator.js
@@ -0,0 +1,10 @@
+const b = 10;
+const c = 20;
+let Plus = c + b;
+let Minus = b - c;
+let Multiply = b * c;
+let Divide = b / c;
+    console.log("Plus:", Plus);
+    console.log("Minus", Minus);
+    console.log("Multiply", Multiply);
+    console.log("Divide:", Divide);
---
+++ countdown.js
@@ -0,0 +1,2 @@
+for (var x = 4 - 1; x >= 0; x--)
+    console.log(x);
